5338da13cf05a713ce44dffb0c2bc8d50d06b4bc
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2017-10-24 17:23:06 +0200

    feat(connection-manager): release connection manager

---
+++ src/__mocks__/AbstractConnection.js
@@ -0,0 +1,23 @@
+const OriginalAbstractConnection = require.requireActual("@dcos/connections")
+  .AbstractConnection;
+
+const AbstractConnection = function(url) {
+  this.url = url;
+  this.state = OriginalAbstractConnection.INIT;
+  this.open = jest.fn(() => {
+    this.state = OriginalAbstractConnection.OPEN;
+  });
+  this.close = jest.fn(() => {
+    this.state = OriginalAbstractConnection.CLOSED;
+  });
+  this.on = jest.fn();
+  this.addListener = jest.fn();
+  this.removeListener = jest.fn();
+  this.listeners = jest.fn(() => []);
+};
+
+AbstractConnection.INIT = OriginalAbstractConnection.INIT;
+AbstractConnection.OPEN = OriginalAbstractConnection.OPEN;
+AbstractConnection.CLOSED = OriginalAbstractConnection.CLOSED;
+
+export default AbstractConnection;
